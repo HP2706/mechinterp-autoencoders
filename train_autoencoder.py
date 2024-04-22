fd3e8c139254965b8f7fbbff73a725f7eb7bc4d2
HP2706 <hprjdk@gmail.com>
2024-04-22 19:52:28 +0200

    train code for autoencoder

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -1,16 +1,9 @@
-from transformers import AutoTokenizer
-import pandas as pd
-from torch.optim import AdamW
-from datamodels import ActivationData
-from typing import Any, Dict, List, Type, Union, Generator
 from utils import get_model_memory_usage, lm_cross_entropy_loss, get_gpu_memory_usage
-import torch
 from common import (
     stub, vol, image, 
     MODELS_DIR,PATH, DATASET_NAME
 )
 from autoencoder import AutoencoderConfig, AutoEncoder
-from mechninterp_utils import get_recons_loss
 from utils import load_activations
 from modal import gpu
 import modal
@@ -22,14 +15,16 @@ MODEL_PATH = f"{MODELS_DIR}/thesephist-contra-bottleneck-t5-small-wikipedia"
 
 with image.imports():
     from transformers import AutoTokenizer, AutoModelForCausalLM
-    from datasets import load_from_disk, Dataset
+    from datasets import load_from_disk
     from huggingface_hub import snapshot_download
+    import torch
+    from torch.optim import AdamW
+    from typing import Any, Dict, List, Type, Union, Generator
+    import wandb
     from tqdm import tqdm
-    import pandas as pd
     import psutil
     import time
     import os
-    import numpy as np
     import json
 
 version = "small"
@@ -200,11 +195,24 @@ def create_activations_dataset():
         print(f"Finished processing {name} dataset in {time_taken / 60} minutes for {orig_size} samples")
 
 
+def save_model(model, cfg, model_path):
+    torch.save(model.state_dict(), f"{model_path}/model.pth")
+    with open(f"{model_path}/config.json", "w") as f:
+        json.dump(cfg.dict(), f)
+
+def load_model(model_path):
+    with open(f"{model_path}/config.json", "r") as f:
+        cfg = AutoencoderConfig.model_validate_json(f.read())
+    model = AutoEncoder(cfg)
+    model.load_state_dict(torch.load(f"{model_path}/model.pth"))
+    return model, cfg
+
 @stub.function(
     image = image,
     volumes={PATH: vol},   
     timeout=60*60, 
     gpu=gpu.A10G(),    
+    secrets=[modal.Secret.from_name("my-wandb-secret")],
 )
 def train_autoencoder():
     activations_name = f"activations_{model_name.replace('/', '-')}_{DATASET_NAME.replace('/', '_')}"
@@ -217,9 +225,9 @@ def train_autoencoder():
         os.makedirs(model_dir)
     
     d_mlp = 512 #TODO this should not be hardcoded
-    model = AutoEncoder(AutoencoderConfig(
+    cfg = AutoencoderConfig(
         seed=42,
-        batch_size=512,
+        batch_size=512*10,
         buffer_mult=10,
         lr=1e-3,
         l1_coeff=0.01,
@@ -232,38 +240,57 @@ def train_autoencoder():
         buffer_size=1000000,
         buffer_batches=25,
         device="cuda",
-        n_epochs=1
-    ))
+        n_epochs=10
+    )
+
+    model = AutoEncoder(cfg)
+    wandb.init(
+        # set the wandb project where this run will be logged
+        project="Sparse AutoEncoder",
+        # track hyperparameters and run metadata
+        config={
+            "learning_rate": cfg.lr,
+            "architecture": "1Layer Transformer",
+            "dataset": "TinyStories",
+            "epochs": cfg.n_epochs,
+        }
+    )
 
     model.to(model.cfg.device)
     optimizer = AdamW(model.parameters(), lr=1e-3)
-    for train_file in [ file for file in os.listdir(f"{PATH}/{activations_name}") if "train" in file]:
-        if not train_file.endswith(".parquet"):
-            continue
+    for epoch in range(model.cfg.n_epochs):
+        for train_file in [ file for file in os.listdir(f"{PATH}/{activations_name}") if "train" in file]:
+            if not train_file.endswith(".parquet"):
+                continue
 
-        df = pd.read_parquet(f"{PATH}/{activations_name}/{train_file}")
-        activations_batched = load_activations(df, model.cfg.batch_size)
+            df = pd.read_parquet(f"{PATH}/{activations_name}/{train_file}")
+            activations_batched = load_activations(df, model.cfg.batch_size)
 
-        for activations in activations_batched:
-            loss, x_reconstruct, acts, l2_loss, l1_loss = model.forward(
-                activations.to(model.cfg.device), method='with_loss'
-            )
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            print(f"loss {loss.item()}")
+            for activations in activations_batched:
+                loss, x_reconstruct, acts, l2_loss, l1_loss = model.forward(
+                    activations.to(model.cfg.device), method='with_loss'
+                )
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                print(f"epoch {epoch} train loss {loss.item()}")
 
-    for eval_file in [file for file in os.listdir(f"{PATH}/{activations_name}") if "validation" in file]:
-        if not eval_file.endswith(".parquet"):
-            continue
-        df = pd.read_parquet(f"{PATH}/{activations_name}/{eval_file}")
-        activations_batched = load_activations(df, model.cfg.batch_size)
-        for activations in activations_batched:
-            loss, x_reconstruct, acts, l2_loss, l1_loss = model.forward(
-                activations.to(model.cfg.device), method='with_loss'
-            )
-            print(f"validation loss {loss.item()}")
+        for eval_file in [file for file in os.listdir(f"{PATH}/{activations_name}") if "validation" in file]:
+            if not eval_file.endswith(".parquet"):
+                continue
+            df = pd.read_parquet(f"{PATH}/{activations_name}/{eval_file}")
+            activations_batched = load_activations(df, model.cfg.batch_size)
+            for activations in activations_batched:
+                with torch.no_grad():
+                    loss, x_reconstruct, acts, l2_loss, l1_loss = model.forward(
+                        activations.to(model.cfg.device), method='with_loss'
+                    )
+                    print(f"validation loss {loss.item()}")
 
-
+    save_model(model, model.cfg, model_dir)
+    print("model saved")
+    print("checking if model can be loaded")
+    model, cfg = load_model(model_dir)
+    print("model loaded")
 
         
