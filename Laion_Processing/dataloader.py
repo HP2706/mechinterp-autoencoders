added histogram + fixed dataset normalization

Laion_Processing/dataloader.py
@@ -1,12 +1,11 @@
 import os
 import torch
 from typing import Any, overload
-import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from typing import Generator, Literal, Optional, List, Tuple, Union
-from pydantic import BaseModel
 from utils import load_tensor
+import math 
 
 def check_inputs(kwargs):
     split = kwargs.get('split', None)
@@ -29,7 +28,7 @@ class LaionDataset(Dataset):
         d_hidden : Optional[float] = None,
     ):
         check_inputs(locals())
-        self.n_sqrt = np.sqrt(d_hidden) if d_hidden is not None else None
+        self.d_hidden = d_hidden
         self.with_filenames = with_filenames
         emb_paths = [os.path.join(emb_folder, f) for f in os.listdir(emb_folder)]
         if len(emb_paths) == 0:
@@ -100,12 +99,12 @@ class LaionDataset(Dataset):
         metadata_df = pd.read_parquet(self.metadata_paths[idx])
         return tensor, metadata_df
             
-    def scale_dataset(self, X : torch.Tensor, n : int):
-        '''computes the expected norm of the dataset row (dim=-1) and normalized to n_sqrt'''
+    def scale_dataset(self, X: torch.Tensor, n: float):
+        '''Computes the expected norm of the dataset row (dim=-1) and normalizes to sqrt(target_norm).'''
+        n_sqrt = math.sqrt(n)
         norms = torch.norm(X, dim=-1, p=2)  # Compute L2 norm of each row
-        mean_norm = torch.mean(norms) 
-        desired_norm = torch.sqrt(torch.tensor(n).float())
-        scaling_factor = desired_norm / mean_norm
+        mean_norm = torch.mean(norms).float()
+        scaling_factor = n_sqrt / mean_norm
         X_scaled = X * scaling_factor  # Scale the dataset
         return X_scaled
 
@@ -114,8 +113,8 @@ class LaionDataset(Dataset):
         if self.current_file_index < len(self.emb_paths):
             self.data = load_tensor(self.emb_paths[self.current_file_index])
 
-            if self.n_sqrt:
-                self.data = self.scale_dataset(self.data, self.n_sqrt)
+            if self.d_hidden:
+                self.data = self.scale_dataset(self.data, self.d_hidden)
 
             if self.return_tuple:
                 self.metadata_df = pd.read_parquet(self.metadata_paths[self.current_file_index])

autoencoder.py
@@ -1,9 +1,9 @@
 import json
+import math
 import pandas as pd
 from typing_extensions import Self
 import os
 from typing import Optional
-from click import Option
 import torch
 from torch import Tensor
 import torch.nn as nn
@@ -11,12 +11,16 @@ from pydantic import BaseModel, model_validator, Field
 from torch.nn import functional as F
 from typing import Tuple, Union, Literal, List, Any, Type, cast, TypeVar
 from abc import ABC, abstractmethod
-from mechninterp_utils import utils
 from utils import get_device
-from mechninterp_utils import mean_ablate_hook
 from typing import overload, Protocol
 from common import stub, vol, image, PATH, dataset_vol, LAION_DATASET_PATH
-from modal import gpu, enter, method
+
+def compute_did_fire(acts : Tensor)-> Tensor:
+    '''compute count of how often specific feature was nonzero across the batch'''
+    return (acts > 0).long().sum(dim=0).cpu()
+
+def compute_avg_num_firing(x: Tensor) -> Tensor:
+    return (x > 0).float().sum(dim=1).mean(0)
 
 def compute_l0_norm(x: torch.Tensor) -> torch.Tensor:
     '''the mean l0 norm of the activations over the batch'''
@@ -38,10 +42,6 @@ def compute_normalized_mse(x : torch.Tensor, ground_truth : torch.Tensor) -> tor
     '''normalized mean squared error by '''
     return ((x-ground_truth).pow(2) / (ground_truth**2).sum(dim=-1, keepdim=True)).sqrt().mean()
 
-def compute_avg_num_firing(x: Tensor) -> Tensor:
-    return (x > 0).float().sum(dim=1).mean(0)
-
-
 class BasicStats(BaseModel):
     loss: Tensor
     x_reconstruct: Tensor
@@ -51,18 +51,24 @@ class BasicStats(BaseModel):
     l_sparsity: Tensor = Field(..., description="the sum of the absolute values of the activations")
     l0_norm: Tensor = Field(..., description="average number of non-zero entries in the activations")
     normalized_mse : Tensor 
+    did_fire : Tensor = Field(..., description="test with ones and zeros for whether metric for whether activation fired or not")
+    avg_num_firing : Tensor = Field(..., description="average number of times a neuron fires")
+    weight_stats : dict[str, float] = Field(..., description="the norms and sums of all the weights")
+
 
     class Config:
         arbitrary_types_allowed = True
 
-    def format_loss(self):
+    def format_data(self):
         return {
             "loss": self.loss.item(),
             "l2_loss": self.l2_loss.item(),
             "l1_loss": self.l1_loss.item(),
             "l0_norm": self.l0_norm.item(),
             "normalized_mse": self.normalized_mse.item(),
-            "l_sparsity": self.l_sparsity.item()
+            "l_sparsity": self.l_sparsity.item(),
+            "avg_num_firing": self.avg_num_firing.item(),
+            **self.weight_stats
         }
 
 
@@ -73,8 +79,8 @@ class GatedAutoEncoderResult(BasicStats):
     class Config:
         arbitrary_types_allowed = True
 
-    def format_loss(self):
-        base_loss = super().format_loss()  # Call the superclass method
+    def format_data(self):
+        base_loss = super().format_data()  # Call the superclass method
         return {
             **base_loss,
             "L_aux": self.L_aux.item(), 
@@ -85,8 +91,8 @@ class AutoencoderResult(BasicStats):
     class Config:
         arbitrary_types_allowed = True
 
-    def format_loss(self):
-        return super().format_loss()
+    def format_data(self):
+        return super().format_data()
         
 
 class AutoencoderModelConfig(BaseModel):
@@ -97,6 +103,7 @@ class AutoencoderModelConfig(BaseModel):
     seed: int
     enc_dtype: Literal['fp32', 'fp16'] = 'fp32'
     device: Literal['cuda', 'mps'] = 'cuda'
+    updated_anthropic_method : bool = True
     
     @property 
     def dtype(self):
@@ -194,6 +201,11 @@ class AutoEncoderBase(nn.Module, ABC):
         """
         pass
 
+    @torch.no_grad()
+    @abstractmethod
+    def get_weight_data(self)-> dict[str, float]:
+        pass
+
     @classmethod
     def get_name(cls) -> str:
         return cls.__name__.lower()
@@ -218,7 +230,7 @@ class AutoEncoderBase(nn.Module, ABC):
         a .json extension.
         """
         if not os.path.isdir(dir_path):
-            raise ValueError(f"Checkpoint file not found at {dir_path}")
+            raise ValueError(f"Got path to file {dir_path} not folder")
             
         json_path = f'{dir_path}/config.json'
         
@@ -241,17 +253,11 @@ class AutoEncoderBase(nn.Module, ABC):
         model.load_state_dict(torch.load(f'{dir_path}/model.pt', map_location=device))
         return model
 
-
     @torch.no_grad()
-    def remove_parallel_component(self) -> None:
-        parallel_component = (self.W_dec.grad * self.W_dec).sum(dim=1, keepdim=True)
-        self.W_dec.grad -= parallel_component * self.W_dec
-
-    @torch.no_grad()
-    def remove_parallel_component_einsum(self) -> None:
-        parallel_component = torch.einsum("ij,ij->i", self.W_dec.grad, self.W_dec)
-        self.W_dec.grad -= torch.einsum("i,ij->ij", parallel_component, self.W_dec)
-
+    def remove_parallel_component_of_grads(self):
+        W_dec_normed = self.W_dec / self.W_dec.norm(dim=-1, keepdim=True)
+        W_dec_grad_proj = (self.W_dec.grad * W_dec_normed).sum(-1, keepdim=True) * W_dec_normed
+        self.W_dec.grad -= W_dec_grad_proj
 
     @classmethod
     @abstractmethod
@@ -265,14 +271,27 @@ class AutoEncoder(AutoEncoderBase):
         d_hidden = cfg.d_mlp * cfg.dict_mult
         torch.manual_seed(cfg.seed)
 
-        #initialization as described in https://transformer-circuits.pub/2024/april-update/index.html#training-saes
-
-        self.W_dec = nn.Parameter(torch.nn.init.kaiming_uniform_(torch.empty(d_hidden, cfg.d_mlp, dtype=cfg.dtype)))
-        norms = torch.norm(self.W_dec, dim=0, keepdim=True)
-        desired_norms = torch.rand(1, cfg.d_mlp, dtype=cfg.dtype) * 0.95 + 0.05  # Random norms between 0.05 and 1
-        self.W_dec.data = self.W_dec.data * (desired_norms / norms)
-
-        self.W_enc = nn.Parameter(self.W_dec.data.t().clone())
+        if not cfg.updated_anthropic_method:
+            #in the first autoencoder we constrain the W_DEC to unit norm     
+            #initialization as described in https://transformer-circuits.pub/2024/april-update/index.html#training-saes   
+            self.W_dec = nn.Parameter(torch.nn.init.kaiming_uniform_(torch.empty(d_hidden, cfg.d_mlp, dtype=cfg.dtype)))
+            with torch.no_grad():
+                # Anthropic normalize this to have unit columns
+                self.W_dec.data /= torch.norm(self.W_dec.data, dim=1, keepdim=True)
+            self.W_enc = nn.Parameter(self.W_dec.data.t().clone())
+        else:
+            # Initialize W_dec with fixed L2 norm of 0.1
+            self.W_dec = nn.Parameter(torch.empty(d_hidden, cfg.d_mlp, dtype=cfg.dtype))
+            nn.init.kaiming_uniform_(self.W_dec, a=math.sqrt(5))
+            with torch.no_grad():
+                #we initialize the weights to have a fixed L2 norm of 0.1
+                self.W_dec.data *= 0.1 / torch.norm(self.W_dec.data, dim=0, keepdim=True)
+            # Initialize W_enc to W_dec^T
+            self.W_enc = nn.Parameter(self.W_dec.data.t().clone())
+        
+        
+        
+        self.relu = nn.ReLU()
        
         self.b_enc = nn.Parameter(torch.zeros(d_hidden, dtype=cfg.dtype)) # initialize to zero
         self.b_dec = nn.Parameter(torch.zeros(cfg.d_mlp, dtype=cfg.dtype)) # initialize to zero
@@ -298,7 +317,6 @@ class AutoEncoder(AutoEncoderBase):
     def load_from_checkpoint(
         cls, 
         checkpoint_path : str, 
-        json_path : Optional[str] = None
     ) -> 'AutoEncoder': ...
 
     @classmethod
@@ -312,10 +330,22 @@ class AutoEncoder(AutoEncoderBase):
     def load_from_checkpoint(
         cls, 
         checkpoint_path : str, 
-        json_path : Optional[str] = None
     ) -> 'AutoEncoder': 
         
         return super(AutoEncoder, cls).load_from_checkpoint(checkpoint_path) #type: ignore
+
+    def get_weight_data(self)-> dict[str, float]:
+        return {
+            'W_enc_sum': self.W_enc.sum().item(),
+            'b_enc_sum': self.b_enc.sum().item(),
+            'W_dec_sum': self.W_dec.sum().item(),
+            'b_dec_sum': self.b_dec.sum().item(),
+            'W_enc_norm': torch.norm(self.W_enc).item(),
+            'b_enc_norm': torch.norm(self.b_enc).item(),
+            'W_dec_norm': torch.norm(self.W_dec).item(),
+            'b_dec_norm': torch.norm(self.b_dec).item(),
+        }
+
 
     def get_single_feature_acts(
         self,     
@@ -359,14 +389,24 @@ class AutoEncoder(AutoEncoderBase):
             b_dec = self.b_dec
 
         x_center = x - b_dec
-        acts = F.relu(x_center @ W_enc + b_enc)
+        acts = self.relu(x_center @ W_enc + b_enc)
         if method == 'with_acts':
             return acts
         
         x_reconstruct = acts @ W_dec + b_dec
-        if method == 'with_loss':
+        print("acts @ W_dec", (acts @ W_dec).sum(1).mean())
+        print("b_dec", b_dec.sum())
+        print("l1 diff b_dec-x-reconstruct", (b_dec - x_reconstruct).sum())
+        print("self.W_enc", self.W_enc.sum())
+        print("self.b_enc", self.b_enc.sum())
+        print("self.W_dec", self.W_dec.sum())
+        print("self.b_dec", self.b_dec.sum())
+        if method in ['with_loss', 'with_new_loss']:
             l2_loss = compute_mse(x_reconstruct, x)
-            L_sparse = compute_l1_sparsity(acts)
+            if method == 'with_loss':
+                L_sparse = compute_l1_sparsity(acts)
+            else:  # method == 'with_new_loss'
+                L_sparse = (acts.abs() * torch.norm(W_dec, dim=-1)).sum()  # alternative sparsity metric
             loss = l2_loss + (self.l1_coeff * L_sparse)
             
             return AutoencoderResult(
@@ -377,26 +417,14 @@ class AutoEncoder(AutoEncoderBase):
                 l_sparsity=L_sparse,
                 l1_loss=compute_mean_absolute_error(x_reconstruct, x),
                 l0_norm=compute_l0_norm(acts),
-                normalized_mse=compute_normalized_mse(x_reconstruct, x)
+                normalized_mse=compute_normalized_mse(x_reconstruct, x),
+                did_fire=compute_did_fire(acts),
+                avg_num_firing=compute_avg_num_firing(acts),
+                weight_stats=self.get_weight_data()
             )
         elif method == 'reconstruct':
             return x_reconstruct
-        elif method == 'with_new_loss':
-            l2_loss = compute_mse(x_reconstruct, x)
-            L_sparse = (acts.abs() * torch.norm(W_dec, dim=-1)).sum() # alternativ sparsity metric
-            loss = l2_loss + (self.l1_coeff * L_sparse)
-            return AutoencoderResult(
-                loss=loss, 
-                x_reconstruct=x_reconstruct, 
-                acts=acts, 
-                l2_loss=l2_loss, 
-                l1_loss=compute_mean_absolute_error(x_reconstruct, x),
-                l_sparsity=L_sparse,
-                l0_norm=compute_l0_norm(acts),
-                normalized_mse=compute_normalized_mse(x_reconstruct, x)
-            )
-        else:
-            return x_reconstruct
+       
 
 #from paper https://arxiv.org/pdf/2404.16014
 class GatedAutoEncoder(AutoEncoderBase):
@@ -469,7 +497,22 @@ class GatedAutoEncoder(AutoEncoderBase):
 
         return feature_activation.unsqueeze(-1) #[batch, 1]
 
-    
+    def get_weight_data(self)-> dict[str, float]:
+        return {
+            'W_gate_sum': self.W_gate.sum().item(),
+            'b_gate_sum': self.b_gate.sum().item(),
+            'W_mag_sum': self.W_mag.sum().item(),
+            'b_mag_sum': self.b_mag.sum().item(),
+            'W_dec_sum': self.W_dec.sum().item(),
+            'b_dec_sum': self.b_dec.sum().item(),
+            'W_gate_norm': torch.norm(self.W_gate).item(),
+            'b_gate_norm': torch.norm(self.b_gate).item(),
+            'W_mag_norm': torch.norm(self.W_mag).item(),
+            'b_mag_norm': torch.norm(self.b_mag).item(),
+            'W_dec_norm': torch.norm(self.W_dec).item(),
+            'b_dec_norm': torch.norm(self.b_dec).item(),
+        }
+
 
     @overload
     def forward(self, x: Tensor, method: Literal['with_acts', 'reconstruct']) -> Tensor: ...
@@ -513,6 +556,13 @@ class GatedAutoEncoder(AutoEncoderBase):
             L_aux = compute_mse(x, via_gate_reconstruction)
 
             # Summing up the losses
+            print("acts @ W_dec", (acts @ self.W_dec).sum(1).mean())
+            print("b_dec", self.b_dec.sum())
+            print("l1 diff b_dec-x-reconstruct", (self.b_dec - x_reconstruct).sum())
+            print("self.W_enc", self.W_gate.sum())
+            print("self.b_enc", self.b_gate.sum())
+            print("self.W_dec", self.W_dec.sum())
+            print("self.b_dec", self.b_dec.sum())
             loss = l2 + (self.l1_coeff * L_Sparsity) + L_aux
 
             return GatedAutoEncoderResult(
@@ -524,7 +574,10 @@ class GatedAutoEncoder(AutoEncoderBase):
                 x_reconstruct=x_reconstruct, 
                 acts=acts, 
                 L_aux=L_aux,
-                normalized_mse=compute_normalized_mse(x_reconstruct, x)
+                normalized_mse=compute_normalized_mse(x_reconstruct, x),
+                did_fire=compute_did_fire(acts),
+                avg_num_firing=compute_avg_num_firing(acts),
+                weight_stats=self.get_weight_data()
             )
         else:
             return x_reconstruct

mechninterp_utils.py
@@ -1,20 +1,160 @@
 import torch
 from functools import partial
 from transformer_lens import utils
+from typing import Union
+from autoencoder import AutoEncoder, GatedAutoEncoder
 from torchmetrics.regression import SpearmanCorrCoef
+import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
 
-def compute_proxy(proba_distb : torch.Tensor, feature_tokens : torch.Tensor): #NOT finished
-    # shape of proba_distb is [batch, seq_len, vocab_size]
-    # shape of feature_tokens is [n_features]
-    '''assumes we get a full probability distribution over the vocabulary'''
-    assert proba_distb.sum(dim=-1) - 1 < 1e-5
-    #compute p(s)
-    p_s = torch.prod(proba_distb, dim=-1)  # [batch, seq_len]
-    #compute p(s|feature)
-    p_feature_given_s = proba_distb.gather(dim=-1, index=feature_tokens.unsqueeze(-1)).squeeze(-1)  # [batch, seq_len, n_features]
-    p_feature_given_s = p_feature_given_s.prod(dim=-1)  # [batch, seq_len]
-    return p_s, p_feature_given_s  # [batch, seq_len]
+#code for plotting histogram taken from https://github.com/ArthurConmy/sae/tree/8bf510d9285eb5d79f77fe6896f2166d35f06a2b
+# Define a set of arguments which are passed to fig.update_layout (rather than just being included in e.g. px.imshow)
+UPDATE_LAYOUT_SET = {
+    "xaxis_range", "yaxis_range", "hovermode", "xaxis_title", "yaxis_title", "colorbar", "colorscale", "coloraxis", "title_x", "bargap", "bargroupgap", "xaxis_tickformat",
+    "yaxis_tickformat", "title_y", "legend_title_text", "xaxis_showgrid", "xaxis_gridwidth", "xaxis_gridcolor", "yaxis_type", "yaxis_showgrid", "yaxis_gridwidth", "yaxis_gridcolor",
+    "showlegend", "xaxis_tickmode", "yaxis_tickmode", "xaxis_tickangle", "yaxis_tickangle", "margin", "xaxis_visible", "yaxis_visible", "bargap", "bargroupgap", "font",
+    "modebar_add", "legend_traceorder", "autosize", "coloraxis_colorbar_tickformat", "font_family", "font_size",
+}
+# Gives options to draw on plots, and remove plotly logo
+CONFIG = {'displaylogo': False}
+CONFIG_STATIC = {'displaylogo': False, 'staticPlot': True}
+MODEBAR_ADD = ['drawline', 'drawopenpath', 'drawclosedpath', 'drawcircle', 'drawrect', 'eraseshape']
 
+def to_numpy(tensor):
+    """
+    Helper function to convert a tensor to a numpy array. Also works on lists, tuples, and numpy arrays.
+    """
+    if isinstance(tensor, np.ndarray):
+        return tensor
+    elif isinstance(tensor, (list, tuple)):
+        array = np.array(tensor)
+        return array
+    elif isinstance(tensor, (torch.Tensor, torch.nn.parameter.Parameter)):
+        return tensor.detach().cpu().numpy()
+    elif isinstance(tensor, (int, float, bool, str)):
+        return np.array(tensor)
+    else:
+        raise ValueError(f"Input to to_numpy has invalid type: {type(tensor)}")
+
+
+def hist(tensor : torch.Tensor, renderer=None, **kwargs):
+    kwargs_post = {k: v for k, v in kwargs.items() if k in UPDATE_LAYOUT_SET}
+    kwargs_pre = {k: v for k, v in kwargs.items() if k not in UPDATE_LAYOUT_SET}
+    draw = kwargs_pre.pop("draw", True)
+    static = kwargs_pre.pop("static", False)
+    return_fig = kwargs_pre.pop("return_fig", False)
+
+
+    if "modebar_add" not in kwargs_post:
+        kwargs_post["modebar_add"] = ['drawline', 'drawopenpath', 'drawclosedpath', 'drawcircle', 'drawrect', 'eraseshape']
+    add_mean_line = kwargs_pre.pop("add_mean_line", False)
+    names = kwargs_pre.pop("names", None)
+    if "barmode" not in kwargs_post:
+        kwargs_post["barmode"] = "overlay"
+    if "bargap" not in kwargs_post:
+        kwargs_post["bargap"] = 0.0
+    if "margin" in kwargs_post and isinstance(kwargs_post["margin"], int):
+        kwargs_post["margin"] = dict.fromkeys(list("tblr"), kwargs_post["margin"])
+    if "hovermode" not in kwargs_post:
+        kwargs_post["hovermode"] = "x unified"
+    if "autosize" not in kwargs_post:
+        kwargs_post["autosize"] = False
+    # print(kwargs_pre, "and", kwargs_post)
+    arr = to_numpy(tensor)
+    fig = px.histogram(x=arr, **kwargs_pre)
+    fig.update_layout(**kwargs_post)
+    if add_mean_line:
+        if arr.ndim == 1:
+            fig.add_vline(x=arr.mean(), line_width=3, line_dash="dash", line_color="black", annotation_text=f"Mean = {arr.mean():.3f}", annotation_position="top")
+        elif arr.ndim == 2:
+            for i in range(arr.shape[0]):
+                fig.add_vline(x=arr[i].mean(), line_width=3, line_dash="dash", line_color="black", annotation_text=f"Mean = {arr.mean():.3f}", annotation_position="top")
+    if names is not None:
+        for i in range(len(fig.data)): # type: ignore
+            fig.data[i]["name"] = names[i // 2 if "marginal" in kwargs_pre else i] #type: ignore
+    if draw: 
+        fig.update_layout(modebar_add=MODEBAR_ADD)
+    else:
+        fig.update_layout(modebar_add=[])
+    if return_fig:
+        return fig
+    else:
+        fig.show(renderer=renderer, config=CONFIG_STATIC if static else CONFIG)
+
+def anthropic_resample(
+    self,
+    indices,
+    model : Union[AutoEncoder, GatedAutoEncoder]
+):
+    #TODO implement this 
+    
+    anthropic_iterator = range(0, self.cfg["anthropic_resample_batches"], self.cfg["batch_size"])
+    total_size = len(anthropic_iterator) * self.cfg["batch_size"] * self.cfg["seq_len"]
+    anthropic_iterator = tqdm(anthropic_iterator, desc="Anthropic loss calculating")
+    global_loss_increases = torch.zeros((self.cfg["anthropic_resample_batches"],), dtype=self.dtype, device=self.device)
+    global_input_activations = torch.zeros((self.cfg["anthropic_resample_batches"], self.d_in), dtype=self.dtype, device=self.device)
+
+    for refill_batch_idx_start in anthropic_iterator:
+        #TODO get test set
+
+
+        # Do a forwards pass, including calculating loss increase
+
+        normal_loss = None
+
+        normal_loss = normal_loss.cpu()
+        changes_in_loss = sae_loss - normal_loss
+        changes_in_loss_dist = Categorical(
+            torch.nn.functional.relu(changes_in_loss) / torch.nn.functional.relu(changes_in_loss).sum(dim=1, keepdim=True)
+        )
+        samples = changes_in_loss_dist.sample()
+        assert samples.shape == (self.cfg["batch_size"],), f"{samples.shape=}; {self.cfg['batch_size']=}"
+        
+        global_loss_increases[
+            refill_batch_idx_start: refill_batch_idx_start + self.cfg["batch_size"]
+        ] = changes_in_loss[torch.arange(self.cfg["batch_size"]), samples]
+        global_input_activations[
+            refill_batch_idx_start: refill_batch_idx_start + self.cfg["batch_size"]
+        ] = normal_activations[torch.arange(self.cfg["batch_size"]), samples]
+
+    sample_indices = torch.multinomial(
+        global_loss_increases / global_loss_increases.sum(),
+        len(indices), 
+        replacement=False,
+    )
+
+    # Replace W_dec with normalized versions of these
+    self.W_dec.data[indices, :] = (
+        (
+            global_input_activations[sample_indices]
+            / torch.norm(global_input_activations[sample_indices], dim=1, keepdim=True)
+        )
+        .to(self.dtype)
+        .to(self.device)
+    )
+
+    # Set W_enc equal to W_dec.T in these indices, first
+    self.W_enc.data[:, indices] = self.W_dec.data[indices, :].T
+
+    # Then, change norms to be equal to a factor (0.2 in Anthropic) times the average norm of all the other columns, if other columns exist
+    if indices.shape[0] < self.d_sae:
+        sum_of_all_norms = torch.norm(self.W_enc.data, dim=0).sum()
+        sum_of_all_norms -= len(indices)
+        average_norm = sum_of_all_norms / (self.d_sae - len(indices))
+        metrics["resample_norm_thats_hopefully_less_or_around_one"] = average_norm.item()
+        self.W_enc.data[:, indices] *= self.cfg["resample_factor"] * average_norm
+
+        # Set biases to resampledvalue
+        relevant_biases = self.b_enc.data[indices].mean()
+        self.b_enc.data[indices] = relevant_biases * self.cfg["bias_resample_factor"]
+
+    else:
+        self.W_enc.data[:, indices] *= self.cfg["resample_factor"]
+
+        self.b_enc.data[indices] = - 5.0
+
+#for antrhopic interpretability paper
 def torch_spearman_correlation(
     predicted : torch.Tensor, 
     actual : torch.Tensor
@@ -27,46 +167,3 @@ def torch_spearman_correlation(
     '''
     spearman = SpearmanCorrCoef()
     return spearman(predicted, actual) 
-
-
-def replacement_hook(mlp_post : torch.Tensor, hook, encoder):
-    mlp_post_reconstr = encoder(mlp_post)[1]
-    return mlp_post_reconstr
-
-def mean_ablate_hook(mlp_post : torch.Tensor, hook):
-    mlp_post[:] = mlp_post.mean([0, 1])
-    return mlp_post
-
-def zero_ablate_hook(mlp_post : torch.Tensor, hook):
-    mlp_post[:] = 0.
-    return mlp_post
-
-@torch.no_grad()
-def get_recons_loss(
-    model, 
-    encoder, 
-    all_tokens : torch.Tensor,
-    model_batch_size: int,
-    num_batches : int = 5
-):
-    
-    loss_list = []
-    for i in range(num_batches):
-        tokens = all_tokens[torch.randperm(len(all_tokens))[:model_batch_size]]
-        loss = model(tokens, return_type="loss")
-        recons_loss = model.run_with_hooks(
-            tokens, return_type="loss", fwd_hooks=[(utils.get_act_name("post", 0), partial(replacement_hook, encoder=encoder))]
-        )
-        # mean_abl_loss = model.run_with_hooks(tokens, return_type="loss", fwd_hooks=[(utils.get_act_name("post", 0), mean_ablate_hook)])
-        zero_abl_loss = model.run_with_hooks(
-            tokens, return_type="loss", fwd_hooks=[(utils.get_act_name("post", 0), zero_ablate_hook)]
-        )
-        loss_list.append((loss, recons_loss, zero_abl_loss))
-    losses = torch.tensor(loss_list)
-    loss, recons_loss, zero_abl_loss = losses.mean(0).tolist()
-
-    print(f"loss: {loss:.4f}, recons_loss: {recons_loss:.4f}, zero_abl_loss: {zero_abl_loss:.4f}")
-    score = ((zero_abl_loss - recons_loss)/(zero_abl_loss - loss))
-    print(f"Reconstruction Score: {score:.2%}")
-    # print(f"{((zero_abl_loss - mean_abl_loss)/(zero_abl_loss - loss)).item():.2%}")
-    return score, loss, recons_loss, zero_abl_loss

train_clip_autoencoder.py
@@ -17,15 +17,14 @@ from Laion_Processing.dataloader import load_loaders
 import wandb
 from autoencoder import AutoEncoderBase, AutoencoderConfig, AutoEncoder, AutoencoderResult, GatedAutoEncoder, GatedAutoEncoderResult
 from utils import load_tensor
+from mechninterp_utils import hist
 from modal import gpu
 import modal
 import os
 from typing import Literal, Optional, Union
-from utils import remove_keys
 
 from torch.optim import AdamW
 from tqdm import tqdm
-
 
 
 def save_model(
@@ -54,13 +53,19 @@ def save_model(
 def train_autoencoder(
     type: Literal['autoencoder', 'gated_autoencoder'], 
     dict_mult : int,
-    steps: int = 100*10**3, # 100 k steps as per anthropic paper
+    steps: int = 10**5, # 100 k steps as per anthropic paper
     save_interval : int = 10**4, # we save the model every save_interval steps
     test_steps : int = 25*10**3,
     with_ramp: bool = True,
+    max_l_coef : float = 5,
     loss_func: Literal['with_new_loss', 'with_loss'] = 'with_loss',
     retrain_path : Optional[str] = None,
+    resampling_interval : Optional[int] = 10**4,
 ):
+    if with_ramp and loss_func == 'with_loss':
+        print("with_ramp does not work for the with_loss loss function disabling it")
+        with_ramp = False
+
     if retrain_path is not None:
         model = AutoEncoderBase.load_from_checkpoint(retrain_path)
         model_dir = retrain_path
@@ -76,8 +81,8 @@ def train_autoencoder(
             seed=42,
             batch_size=4096, #2048 or 4096
             buffer_mult=10,
-            lr=10e-3, #anthropic suggested 
-            l1_coeff=0, # initially 0 but progressively increases to 5
+            lr=0.0012, 
+            l1_coeff=0 if with_ramp else 10e-3, 
             beta1=0.9,
             beta2=0.999,
             dict_mult=dict_mult,
@@ -100,15 +105,24 @@ def train_autoencoder(
         else:
             model = AutoEncoder(cfg)
 
+    def resample_at_step_idx()->bool:
+        if resampling_interval is None:
+            return False
+        else:
+            return step % resampling_interval == 0    
+
 
     model_dir = f"{PATH}/laion2b_autoencoders/{cfg.folder_name}"
     os.makedirs(model_dir, exist_ok=True)
     vol.commit()
 
+
+    wandb.login(key=os.getenv('WANDB_API_KEY'))
     wandb.init(
         # set the wandb project where this run will be logged
         project="Sparse AutoEncoder",
         # track hyperparameters and run metadata
+        name = cfg.folder_name,
         config={
             "learning_rate": cfg.lr,
             "architecture": "AutoEncoder",
@@ -121,30 +135,34 @@ def train_autoencoder(
         batch_size=cfg.batch_size, 
         emb_folder=EMB_FOLDER,
         train_share=0.8,
-        d_hidden = model.W_dec.shape[0],
+        d_hidden = model.W_dec.shape[0] if cfg.updated_anthropic_method else None,
         n_counts=(None, 1)
     )
 
     model.to(model.cfg.device)
-    
     optimizer = AdamW(model.parameters(), lr=cfg.lr)
     if retrain_path is None:
         step = 0
     else:
         step = model.metadata_cfg.n_steps 
     
-    l1_coeff_final = 5
-    #TODO think about effects of retraining here if model.cfg_metadata.l1_coeff is below l1_coeff_final
-    l1_ramp = l1_coeff_final / ((steps // 100)*5 )
+    last_resampling_step_idx = step
+
+
+    #TODO think about effects of retraining here if model.cfg_metadata.l1_coeff is below max_l_coef
+    l1_ramp = max_l_coef / ((steps // 100)*5 )
     # we linearly increase l1_coeff from 0 to 5 
     #over the first 5% of the total steps as per anthropic paper
+
+    running_frequency_counter = torch.zeros(model.W_dec.shape[0], dtype=torch.int)
+
     while step < steps:
         model.train()
         for batch in tqdm(train_loader, total = len(train_loader), desc="dataset training"): 
             step += 1
             # Update l1_coeff linearly over the first 5% of the total steps
             if with_ramp:
-                if model.l1_coeff <= l1_coeff_final:
+                if model.l1_coeff <= max_l_coef:
                     model.l1_coeff += l1_ramp 
 
             batch = batch.to(model.cfg.device)
@@ -152,17 +170,27 @@ def train_autoencoder(
 
             optimizer.zero_grad()
             result = model.forward(batch, method=loss_func) # type: ignore
+
+            running_frequency_counter += result.did_fire
+
             result.loss.backward()
             clip_grad_norm_(model.parameters(), max_norm=1) 
             # clip grad norm as per anthropic https://transformer-circuits.pub/2024/april-update/index.html#training-saes 
-            model.remove_parallel_component()
+            
+            model.remove_parallel_component_of_grads()
             optimizer.step()
             
             #data = remove_keys(result.model_dump(), ['x_reconstruct', 'acts'])
-            wandb.log({
+            metrics = {
                 "l1_coeff": model.l1_coeff,
-                **result.format_loss()
-            })
+                **result.format_data()
+            }
+
+            if not cfg.updated_anthropic_method:
+                with torch.no_grad():
+                    # Renormalize W_dec to have unit norm
+                    norms = torch.norm(model.W_dec.data, dim=1, keepdim=True)
+                    model.W_dec.data /= (norms + 1e-6) 
 
 
             if step % save_interval == 0:
@@ -170,6 +198,28 @@ def train_autoencoder(
                 save_model(model, cfg, model_dir)
             cfg.n_steps = step
             cfg.l1_coeff = model.l1_coeff
+
+            if resample_at_step_idx():
+                current_frequency_counter = running_frequency_counter.to(cfg.device) / (cfg.batch_size * (step - last_resampling_step_idx))
+
+                running_frequency_counter = torch.zeros_like(running_frequency_counter) # reset running counter to all zeros
+                #from https://github.com/ArthurConmy/sae/tree/8bf510d9285eb5d79f77fe6896f2166d35f06a2b)
+                fig = hist(
+                    torch.max(current_frequency_counter.cpu(), torch.FloatTensor([1e-10])).log10().cpu(),
+                    # Show proportion on y axis
+                    histnorm="percent",
+                    title = "Histogram of SAE Neuron Firing Frequency (Proportions of all Neurons)",
+                    xaxis_title = "Log10(Frequency)",
+                    yaxis_title = "Percent (changed!) of Neurons",
+                    return_fig = True,
+                    # Do not show legend
+                    showlegend = False
+                )
+                metrics["frequency_histogram"] = fig
+
+                last_resampling_step_idx = step
+
+            wandb.log(metrics)
 
             if step % test_steps == 0:
                 model.eval()
@@ -179,16 +229,11 @@ def train_autoencoder(
                         result = model.forward(batch, method="with_loss")
                         wandb.log(
                             {
-                                f'test_{key}': value for key, value in result.format_loss().items()
+                                f'test_{key}': value for key, value in result.format_data().items()
                             }
                         )
 
             cfg.n_epochs += 1
-
-def create_hist(
-    acts : torch.Tensor
-):
-    '''creates a historgam plotting ..'''
 
 
 @stub.function(